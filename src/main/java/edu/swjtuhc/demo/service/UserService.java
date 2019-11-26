941482f784c0909ebd2d5f802355884bea3385ef
qinyuanyuan <1289518249@qq.com>
2019-11-26 10:58:10 +0800

    修改2

--- src/main/java/edu/swjtuhc/demo/service/UserService.java
+++ src/main/java/edu/swjtuhc/demo/service/UserService.java
@@ -7,8 +7,5 @@ import edu.swjtuhc.demo.model.Sysuser;
 public interface UserService {
  public int register(Sysuser user);
 
-public static List<Sysuser> findByusernameAndpassword(String username, String password) {
-	// TODO Auto-generated method stub
-	return null;
-}
+public  List<Sysuser> findByusernameAndpassword(String username, String password);
 }
--- src/main/java/edu/swjtuhc/demo/serviceImpl/UserServiceImpl.java
+++ src/main/java/edu/swjtuhc/demo/serviceImpl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package edu.swjtuhc.demo.serviceImpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +17,16 @@ private UserMapper userMapper;
 @Override
 public int register(Sysuser user) {
 	// TODO Auto-generated method stub
+	
 	int i = userMapper.insertUser(user);
 	return i;
+}
+
+@Override
+public List<Sysuser> findByusernameAndpassword(String username, String password) {
+	// TODO Auto-generated method stub
+	
+	
+	return null;
 }
 }

0a04a3f04e4e9927a5264c06c95ab675d44b7821
qinyuanyuan <1289518249@qq.com>
2019-12-05 11:50:34 +0800

    shanchu

--- src/main/java/edu/swjtuhc/demo/model/Sysuser.java
+++ src/main/java/edu/swjtuhc/demo/model/Sysuser.java
@@ -7,78 +7,47 @@ public class Sysuser {
 	private String name;
 	private int age;
 	private String gender;
-	private String image;
-	private String fileName;
-	private String userCode;
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-	public String getUserCode() {
-		return userCode;
-	}
-
-	public void setUserCode(String userCode) {
-		this.userCode = userCode;
-	}
-
+	
 	public int getU_id() {
 		return u_id;
 	}
-
 	public void setU_id(int u_id) {
 		this.u_id = u_id;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+	@Override
+	public String toString() {
+		return "Sysuser [u_id=" + u_id + ", username=" + username + ", password=" + password + ", name=" + name
+				+ ", age=" + age + ", gender=" + gender + "]";
+	}
+	
 }
